867bbd528fc5a495cbae03c8edb8838dab6f7bf5
ismaildedec <ismaildedec@gmail.com>
2021-08-29 01:15:59 +0300

    Static(Statik) ifadesi çalışması eklendi.

---
+++ StaticStatement/StaticStatement/Ogrenci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaticStatement
+{
+    class Ogrenci
+    {
+        public static int Numara { get; set; }
+        public static void Not()
+        {
+            MessageBox.Show("Test");
+            
+        }
+    }
+}
