0d636b39d2dfa655615ea63a4c1bfcca1f55e4e9
ismaildedec <ismaildedec@gmail.com>
2021-09-06 19:54:13 +0300

    Interface çalışması eklendi.

---
+++ Interface/Interface/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtText.Location = new System.Drawing.Point(66, 48);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(131, 200);
+            this.txtText.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ Interface/Interface/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        interface Computer
+        {
+            void GetDesktop ();
+            void GetLaptops();
+        }
+        interface Dellinterface
+        {
+            void GetDelllaptops();
+        }
+        class Dell : Computer,Dellinterface
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public void GetDelllaptops()
+            {
+                Name = "Dell Laptops";
+                Price = 9500;
+            }
+
+            public void GetDesktop() 
+            {
+                Name = "Desktop";
+                Price = 10000;
+            }
+            public void GetLaptops()
+            {
+                Name = "Laptop";
+                Price = 20000;
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dell dell = new Dell();
+            dell.GetDesktop();
+            txtText.Text = dell.Name+""+dell.Price+Environment.NewLine;
+            dell.GetLaptops();
+            txtText.Text += dell.Name + "" + dell.Price + Environment.NewLine;
+            dell.GetDelllaptops();
+            txtText.Text += dell.Name + "" + dell.Price + Environment.NewLine;
+
+
+
+
+
+        }
+    }
+}
